8a21028f788b5e04c6ad80c421f69aacaec7b98a
Borys Matveichuk <borys.matveichuk@temy.co>
2025-01-31 19:04:00 +0200

    Add images for categories

---
+++ lib/utils/images.dart
@@ -0,0 +1,7 @@
+abstract final class OrgansImages {
+  static const String _path = 'assets/images/organs/';
+
+  static const String heart = '${_path}heart.png';
+  static const String lungs = '${_path}lungs.png';
+  static const String brain = '${_path}brain.png';
+}
